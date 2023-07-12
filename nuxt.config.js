de9bbb4e7147ac042f312944befce3c659b6c288
sixmono <2043805383@qq.com>
2023-07-12 16:35:04 +0800

    官网v1.7

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'project',
+    title: 'JT-official',
     htmlAttrs: {
       lang: 'en'
     },
