f783347b766ced273063cca09909864046509e52
sixmono <2043805383@qq.com>
2023-07-12 17:54:06 +0800

    官网v2.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,5 +41,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  target: "static",
+  ssr: false,
+  generate: {
+    dir: "docs",
+    fallback: false
+  },
 }
